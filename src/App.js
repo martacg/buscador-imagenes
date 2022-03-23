c489942beb1ae83e48dfe86f684cd5b1710dd48d
Marta González <martacg7@gmail.com>
2022-03-23 20:44:21 +0100

    Corrección headers App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         onSubmit={async values => {
           const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
             headers: {
-              'Authorization': 'AQUI TU API KEY'
+              'Authorization': 'Client-ID AQUI TU API KEY'
             }
           })
           const data = await response.json()
